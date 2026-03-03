3e151b85438aa5d626a10301b56e6fcf14c8ddbf
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-03 10:38:15 +0300

    Create part-z-0001-variables.go

---
+++ part-z-0001-variables.go
@@ -0,0 +1,16 @@
+package main
+import "fmt";
+
+func main() {
+
+	var year uint16 = 2026 
+
+	year = 2000
+
+	var sales int = 3_250_000 
+
+	name := "Mustafa"
+
+	fmt.Println(year, sales, name) // 2000 3250000 Mustafa
+}
+
