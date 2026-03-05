3eb2979fc6a844e284f3548c07032fe28f56a6f5
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-05 22:37:21 +0300

    Create  part-z-0024-struct1.go

---
+++ part-z-0024-struct1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	type Student struct {
+
+		name string
+		university string
+		age uint16
+	}
+
+	studentAyhan := Student {"Ayhan Bilir", "İstiklal University", 23}
+
+	fmt.Println(studentAyhan)
+	fmt.Println(studentAyhan.name)
+	fmt.Println(studentAyhan.university)
+	fmt.Println(studentAyhan.age)
+}
+
+// {Ayhan Bilir İstiklal University 23}
+// Ayhan Bilir        
+// İstiklal University
+// 23
