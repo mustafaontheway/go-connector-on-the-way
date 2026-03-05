4b785d14c9a7390d206269ee2a88a6b68ae7e5fe
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-05 22:09:29 +0300

    Create part-z-0023-map1.go

---
+++ part-z-0023-map1.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	employees := map[uint8] string {47: "ayhan bilir", 59: "bengü güzel"}
+
+	fmt.Println(employees)
+}
+
+// map[47:ayhan bilir 59:bengü güzel]
