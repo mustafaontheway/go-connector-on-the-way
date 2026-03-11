f51c60e153214d6d4a2e8b202de6c2a8638a7883
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-11 18:39:16 +0300

    Create part-y-0006-digit-extraction2.go

---
+++ part-y-0006-digit-extraction2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a, b, c := findDigits(795)
+
+	fmt.Println(a, b, c) // 5 9 7
+}
+
+func findDigits(num uint) (uint8, uint8, uint8) {
+
+	if num < 100 || num > 999 {return 0, 0, 0}
+
+	ones := num % 10 
+	tens := (num / 10) % 10
+	hundreds := num / 100
+
+	return  uint8(ones), uint8(tens), uint8(hundreds)
+}
