d5f72a211a7536371f8445bc9c7c15c495f17adc
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-11 15:58:31 +0300

    Create part-z-0028-recursive-function2.go

---
+++ part-z-0028-recursive-function2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	getLastYears(2026) // 2026 2025 2024 2023 2022 2021 2020
+
+	getLastYears(2000)
+}
+
+func getLastYears(year uint16) {
+
+	if year >= 2020 {
+
+		fmt.Printf("%d ", year)
+
+		getLastYears(year - 1)
+	}
+}
