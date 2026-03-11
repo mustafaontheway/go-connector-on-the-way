3f9fc0490239d95f91c21f3b32ffdd8f032105d4
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-11 16:03:23 +0300

    Create part-z-0029-function-return-cut1.go

---
+++ part-z-0029-function-return-cut1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	giveSomeInfo()
+}
+
+func giveSomeInfo() {
+
+	fmt.Println("Lorem ipsum 1")
+	fmt.Println("Lorem ipsum 2")
+	fmt.Println("Lorem ipsum 3")
+
+	return
+
+	fmt.Println("Lorem ipsum 4")
+	fmt.Println("Lorem ipsum 5")
+}
+
+/*
+Lorem ipsum 1
+Lorem ipsum 2
+Lorem ipsum 3
+
+*/
