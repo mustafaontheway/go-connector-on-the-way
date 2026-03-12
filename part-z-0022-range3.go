736e0c1640a5906adc055d076305e69aa0d52aed
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-12 21:07:04 +0300

    Create part-z-0022-range3.go

---
+++ part-z-0022-range3.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	ages := []uint8 {37, 21, 96, 87, 15, 96, 74, 5}
+
+	var totalAges uint
+
+	for _, age := range ages {
+
+		totalAges += uint(age)
+	}
+
+	fmt.Println("Average age:", totalAges/ uint(len(ages))) // Average age: 53
+}
+
