6b824941ec7eeb69d7023451b7b1553da0b055fe
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-21 17:02:07 +0300

    Create part-z-0035-interface3.go

---
+++ part-z-0035-interface3.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	rect := Rectangle {width: 12, height: 17}
+
+	circ := Circle {radius: 19}
+
+	calculator(rect)
+
+	calculator(circ)
+
+}
+
+type Geo interface {
+
+	area() uint
+
+	perimeter() uint
+}
+
+type Rectangle struct {
+
+	width, height uint
+}
+
+func (r Rectangle) area() uint {
+
+	return r.height * r.width
+}
+
+func (r Rectangle) perimeter() uint {
+
+	return 2 * (r.height + r.width)
+}
+
+type Circle struct {
+
+	radius uint
+}
+
+func (c Circle) area() uint {
+
+	return 3 * c.radius * c. radius
+}
+
+func (c Circle) perimeter() uint {
+
+	return 6 * c.radius
+}
+
+func calculator(g Geo) {
+
+	fmt.Println(g.area())
+	fmt.Println(g.perimeter())
+}
+
+// 204
+// 58
+// 1083
+// 114
